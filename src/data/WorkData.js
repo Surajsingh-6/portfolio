cb04cdfd014887e1192829338a291e3cb32c8dc7
Suraj singh <suraj142005@gmail.com>
2025-02-03 09:47:50 +0530

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,57 @@
+export const Work = [
+    {
+        id:1,
+        name:"Stock Market Prediction Model",
+        description:"An AI-powered model that predicts stock prices using machine learning techniques like regression and deep learning.",
+        tags:["machine learning","python","scikit-learn","tensorflow"],
+        
+        demo:"https://github.com/Surajsingh-6/Stock-Market-Prediction",
+        github:"https://github.com/Surajsingh-6/Stock-Market-Prediction"
+    },
+    {
+        id:2,
+        name:"E-Commerce Website (Balaji Gift Palace)",
+        description:"A fully functional eCommerce platform built with Next.js, optimized for SEO and high performance.",
+        tags:["nextjs","react","mongodb","nodejs","seo"],
+        
+        demo:"https://balajigiftpalace.live/",
+        github:"https://github.com/Surajsingh-6/BalajiGiftPalace"
+    },
+    {
+        id:3,
+        name:"NEET Quiz Performance Analyzer",
+        description:"An AI-based system that analyzes student quiz performance, providing insights and recommendations for improvement.",
+        tags:["machine learning","python","data visualization","flask"],
+        
+        demo:"https://github.com/Surajsingh-6/Personalized-Student-Recommendations",
+        github:"https://github.com/Surajsingh-6/Personalized-Student-Recommendations"
+    },
+    {
+        id:4,
+        name:"Personal Portfolio Website",
+        description:"A fully responsive and interactive personal portfolio website showcasing my projects and skills.",
+        tags:["nextjs","react","tailwind","framer-motion"],
+        
+        demo:"https://surajsingh-6.github.io/portfolio-mini/",
+        github:"https://github.com/Surajsingh-6/portfolio-mini"
+    },
+    {
+        id:5,
+        name:"AI Chatbot for Government Schemes",
+        description:"An intelligent chatbot that helps users find relevant Indian government schemes and scholarships.",
+        tags:["Html","nlp","dialogflow","mongodb"],
+        
+        demo:"https://tngovai.vercel.app/",
+        github:"https://github.com/Surajsingh-6/ChatBot"
+    },
+    {
+        id:6,
+        name:"AI News Summarizer* (In Progress)",
+        description:"An AI tool that reads daily tech news, summarizes key events, and posts an automated blog update.",
+        tags:["machine learning","python","nlp","fastapi"],
+        
+        demo:"https://github.com/Surajsingh-6/tech-blog",
+        github:"https://github.com/Surajsingh-6/tech-blog"
+    },
+    
+];
